5231f8302bb1204a93fbb3379eb1e928db6b4c03
kurniacf <kcf170202@gmail.com>
2022-05-28 19:19:39 +0700

    feat: soal 2

--- D_5025201073.R
+++ D_5025201073.R
@@ -18,12 +18,12 @@ my_data <- data.frame(
 print(my_data)
 
 # Standar Devisiasi sebelum aktivitas
-SD_before <- sd(before)
-SD_before
+sd_before <- sd(before)
+sd_before
 
 # Standar Devisiasi setelah aktivitas
-SD_after <-sd(after)
-SD_after
+sd_after <- sd(after)
+sd_after
 
 # 1b
 # carilah nilai t (p-value)
@@ -40,8 +40,19 @@ t.test(before, after, alternative = "greater", var.equal = FALSE)
 
 var.test(before, after)
 
-t.test(before, after, var.equal = TRUE)
+t.test(before, after, mu = 0, alternative = "two.sided", var.equal = TRUE)
+
 
 # Soal 2
-# 2a
+install.packages("BSDA")
+library(BSDA)
+
+# Soal 2a
+# Jawaban di README
+
+# Soal 2b 
+tsum.test(mean.x=23500, sd(3900), n.x=100)
+
+# Soal 2c
+# Jawaban di README
 
