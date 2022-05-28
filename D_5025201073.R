5eec4a9f6a7b2a7c54e9c945f7b29c87543a9a1e
kurniacf <kcf170202@gmail.com>
2022-05-28 07:21:54 +0700

    feat: soal 1a-1b

---
+++ D_5025201073.R
@@ -0,0 +1,40 @@
+# Soal 1
+# 1a
+# Carilah Standar Deviasi dari data selisih pasangan pengamatan tabel diatas
+
+# Data Sebelum melakukan aktivitas
+before <- c(78, 75, 67, 77, 70, 72, 28, 74, 77)
+
+# Data Setelah melakukan aktivitas
+after <- c(100, 95, 70, 90, 90, 90, 89, 90, 100)
+
+# Cek data
+my_data <- data.frame(
+    group = rep(c("sebelum", "sesudah"), each = 9),
+    saturation = c(before, after)
+)
+
+# Di print
+print(my_data)
+
+# Standar Devisiasi sebelum aktivitas
+SD_before <- sd(before)
+SD_before
+
+# Standar Devisiasi setelah aktivitas
+SD_after <-sd(after)
+SD_after
+
+# 1b
+# carilah nilai t (p-value)
+
+# Mencari nilai t(p-value) dengan t-test
+t.test(before, after, alternative = "greater", var.equal = FALSE)
+
+# 1c
+# tentukanlah apakah terdapat pengaruh yang signifikan secara statistika
+# dalam hal kadar saturasi oksigen , sebelum dan sesudah melakukan
+# aktivitas ???? jika diketahui tingkat signifikansi ???? = 5% serta H0 : "tidak ada
+# pengaruh yang signifikan secara statistika dalam hal kadar saturasi
+# oksigen , sebelum dan sesudah melakukan aktivitas ????"
+
