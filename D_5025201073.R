c34b3d3c4b2512603507d5dbb9bf3a3d6876bc1d
kurniacf <kcf170202@gmail.com>
2022-05-29 10:44:44 +0700

    feat: soal 3 & 4

--- D_5025201073.R
+++ D_5025201073.R
@@ -56,3 +56,80 @@ tsum.test(mean.x=23500, sd(3900), n.x=100)
 # Soal 2c
 # Jawaban di README
 
+# Soal 3
+# Soal 3a
+# H0 dan H1
+# Jawaban di README
+
+# soal 3b 
+# Hitung Sampel Statistik
+tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, 
+          mean.y =2.79 , s.y = 1.32, n.y = 27, 
+          alternative = "greater", var.equal = TRUE)
+
+# soal 3c
+# Lakukan Uji Statistik (df =2)
+install.packages("mosaic")
+library(mosaic)
+
+plotDist(dist='t', df=2, col="blue")
+
+# soal 3d
+# Nilai Kritikal
+qchisq(p = 0.05, df = 2, lower.tail=FALSE)
+
+# soal 3e
+# Jawaban di README
+
+# soal 3f
+# Jawaban di README
+
+# Soal 4
+# Soal 4a
+# Buatlah masing masing jenis spesies menjadi 3 subjek "Grup" (grup 1,grup
+# 2,grup 3). Lalu Gambarkan plot kuantil normal untuk setiap kelompok dan
+# lihat apakah ada outlier utama dalam homogenitas varians.
+
+myFile  <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"))
+dim(myFile)
+head(myFile)
+attach(myFile)
+
+myFile$V1 <- as.factor(myFile$V1)
+myFile$V1 = factor(myFile$V1,labels = c("Kucing Oren","Kucing Hitam","Kucing Putih","Kucing Oren"))
+
+class(myFile$V1)
+
+group1 <- subset(myFile, V1=="Kucing Oren")
+group2 <- subset(myFile, V1=="Kucing Hitam")
+group3 <- subset(myFile, V1=="Kucing Putih")
+
+# Soal 4b
+# carilah atau periksalah Homogeneity of variances nya , Berapa nilai p yang
+# didapatkan? , Apa hipotesis dan kesimpulan yang dapat diambil ?
+bartlett.test(Length~V1, data=dataoneway)
+
+# Soal 4c
+# Untuk uji ANOVA (satu arah), buatlah model linier dengan Panjang versus
+# Grup dan beri nama model tersebut model 1.
+qqnorm(group1$Length)
+qqline(group1$Length)
+
+# Soal 4d
+# Jawaban di README
+
+# Soal 4e
+model1 <- lm(Length~Group, data=myFile)
+
+anova(model1)
+
+TukeyHSD(aov(model1))
+
+# Soal 4f
+library(ggplot2)
+ggplot(dataoneway, aes(x = Group, y = Length)) + geom_boxplot(fill = "grey80", colour = "black") + 
+  scale_x_discrete() + xlab("Treatment Group") +  ylab("Length (cm)")
+
+
+# Soal 5
+# Soal 5a
